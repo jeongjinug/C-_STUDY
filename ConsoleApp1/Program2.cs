200d858b950cabce28873d492fdbed7b3e8ea247
jeongjinug <scott1379@naver.com>
2022-09-22 23:38:41 +0900

    2장 문제 2-12. 4번

---
+++ ConsoleApp1/Program2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program2
+    {
+        public static void Main() 
+        {
+            Console.Write("섭씨 온도 입력 : ");
+            double C = Double.Parse(Console.ReadLine());
+            double F = 9 / 5 * C + 32;
+            Console.WriteLine("화씨 온도는 {0:F} 입니다.", F);
+        }
+    }
+}

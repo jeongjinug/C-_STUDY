cf8a21088a4e0de899303443f17d01fe80ec312c
jeongjinug <scott1379@naver.com>
2022-12-10 02:03:01 +0900

    v

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -51,7 +51,7 @@ namespace ConsoleApp1
             num3 = Int32.Parse(Console.ReadLine());
             num3 = Int32.Parse(Console.ReadLine());
             num3 = Int32.Parse(Console.ReadLine());
-
+            num3 = Int32.Parse(Console.ReadLine());
 
 
 
